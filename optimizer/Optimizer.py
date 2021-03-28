720c41916494df0af65c76ede425f3b7923ecd9c
lovemefan <450489712@qq.com>
2021-03-28 16:08:26 +0800

    添加一些梯度下降算法

--- optimizer/Optimizer.py
+++ optimizer/Optimizer.py
@@ -31,8 +31,8 @@ class Optimizer():
                 history.append(x)
         return history
 
-    def momentun(self, df, x, alpha=0.01, gamma=0.8, iterations=100, epsilon=1e-6):
-        """Momentum 动量法
+    def gradient_descent_momentun(self, df, x, alpha=0.01, gamma=0.8, iterations=100, epsilon=1e-6):
+        """Momentum 动量法,类似于下落物体的惯性，惯性可以像小球一样，当初速度较大时，可以跳出一些比较小的极值区
         :param df: 导数函数
         :param x: 输入
         :param alpha: 学习率
@@ -59,8 +59,43 @@ class Optimizer():
 
         return history
 
+    def gradient_descent_adagrad(self, df, x, alpha=0.01, iterations: int = 100, epsilon=1e-8):
+        """Adagrad 自适应梯度，
+        :param df: 导数函数
+        :param x: 输入
+        :param alpha: 学习率
+        :param gamma: 超参数
+        :param iterations: 迭代次数
+        :param epsilon:
+        :return: epsilon > 0, 梯度下降的停止条件
+        """
+        assert epsilon > 0, "epsilon must be positive "
+        assert alpha > 0, "alpha must be positive "
+        assert iterations > 0 and type(iterations) == int, "iterations must be positive integer"
+
+        history = []
+        gl = np.zeros_like(x)
+        for i in range(iterations):
+            if np.max(np.abs(df(x))) < epsilon:
+                break
+            else:
+                gl += df(x)**2
+                v = alpha * df(x)/(np.sqrt(gl) + epsilon)
+                x = x - v
+                history.append(x)
+
+        return history
+
+    def gradient_descent_Adadelta(self, df, x, alpha=0.1, rho=0.9, iterations=100, epsilon=1e-8):
+
+        assert epsilon > 0, "epsilon must be positive "
+        assert alpha > 0, "alpha must be positive "
+        assert rho > 0, "rho must be positive "
+        assert iterations > 0 and type(iterations) == int, "iterations must be positive integer"
+
+
 
 if __name__ == '__main__':
     optimizer = Optimizer()
     df = lambda x: np.power(3 * x, 2) - 6 - 10
-    result = optimizer.momentun(df, np.array([1.0, 2]), 0.01, 0.8, 20, 1e-8)
+    result = optimizer.gradient_descent_adagrad(df, np.array([1.0, 2]), 0.01, 200, 1e-8)
--- test/OptimizerTest.py
+++ test/OptimizerTest.py
@@ -21,3 +21,14 @@ class OptimizerTest(unittest.TestCase):
 
         result = self.optimizer.gradient_descent(df, np.array([1.0, 2]), 0.01, 20, 1e-8)
         assert type(result) == list
+
+    def test_momentum(self):
+        df = lambda x: np.power(3 * x, 2) - 6 - 10
+
+        self.assertRaises(AssertionError, self.optimizer.gradient_descent, df, 1.0, -0.01, 200, 1e-8)
+        self.assertRaises(AssertionError, self.optimizer.gradient_descent, df, 1.0, 0.01, -200, 1e-8)
+        self.assertRaises(AssertionError, self.optimizer.gradient_descent, df, 1.0, 0.01, 20.5, 0)
+        self.assertRaises(AssertionError, self.optimizer.gradient_descent, df, 1.0, 0.01, 200, -1)
+
+        result = self.optimizer.momentun(df, np.array([1.0, 2]), 0.01, 0.8, 20, 1e-8)
+        assert type(result) == list
