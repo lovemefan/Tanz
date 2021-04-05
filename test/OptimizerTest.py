871db9a6be71be7f8fbfaf6b2481409b2997df33
lovemefan <450489712@qq.com>
2021-04-05 18:10:19 +0800

    add gradient test

--- test/OptimizerTest.py
+++ test/OptimizerTest.py
@@ -8,11 +8,10 @@ from optimizer.Optimizer import Optimizer
 
 
 class OptimizerTest(unittest.TestCase):
-
     optimizer = Optimizer()
 
     def test_gradient_descent(self):
-        df = lambda x: np.power(3*x, 2) - 6 - 10
+        df = lambda x: 3 * np.power(x, 2) - 9
 
         self.assertRaises(AssertionError, self.optimizer.gradient_descent, df, 1.0, -0.01, 200, 1e-8)
         self.assertRaises(AssertionError, self.optimizer.gradient_descent, df, 1.0, 0.01, -200, 1e-8)
@@ -23,34 +22,35 @@ class OptimizerTest(unittest.TestCase):
         assert type(result) == list
 
     def test_momentum(self):
-        df = lambda x: np.power(3 * x, 2) - 6 - 10
+        df = lambda x: 3 * np.power(x, 2) - 9
 
         self.assertRaises(AssertionError, self.optimizer.gradient_descent, df, 1.0, -0.01, 200, 1e-8)
         self.assertRaises(AssertionError, self.optimizer.gradient_descent, df, 1.0, 0.01, -200, 1e-8)
         self.assertRaises(AssertionError, self.optimizer.gradient_descent, df, 1.0, 0.01, 20.5, 0)
         self.assertRaises(AssertionError, self.optimizer.gradient_descent, df, 1.0, 0.01, 200, -1)
 
-        result = self.optimizer.momentun(df, np.array([1.0, 2]), 0.01, 0.8, 20, 1e-8)
-        assert type(result) == list
+        result = self.optimizer.gradient_descent_momentun(df, np.array([2.0]), 0.01, 0.8, 500, 1e-8)
+        assert np.abs(result[-1] - np.sqrt(3)) < 1e-5
 
     def test_adagrad(self):
-        df = lambda x: np.power(3 * x, 2) - 6 - 10
-        result = self.optimizer.gradient_descent_adagrad(df, np.array([1.0, 2]), 0.01, 200, 1e-8)
-        assert type(result) == list
+        df = lambda x: 3 * np.power(x, 2) - 9
+        result = self.optimizer.gradient_descent_adagrad(df, np.array([2.0]), 0.01, 5000, 1e-8)
+        assert np.abs(result[-1] - np.sqrt(3)) < 1e-5
 
     def test_Adadelta(self):
-        df = lambda x: np.power(3 * x, 2) - 6 - 10
-        result = self.optimizer.gradient_descent_Adadelta(df, np.array([1.0, 2]), 0.01, 0.9, 200, 1e-8)
-        assert type(result) == list
+        df = lambda x: 3 * np.power(x, 2) - 9
+        result = self.optimizer.gradient_descent_Adadelta(df, np.array([2.0]), 0.01, 0.9, 50000, 1e-8)
+        print(np.abs(result[-1] - np.sqrt(3)))
+        assert np.abs(result[-1] - np.sqrt(3)) < 1e-2
 
     def test_RMSprop(self):
-        df = lambda x: np.power(3 * x, 2) - 6 - 10
-        result = self.optimizer.gradient_descent_RMSprop(df, np.array([1.0, 2]), 0.01, 0.9, 200, 1e-8)
-        assert type(result) == list
+        df = lambda x: 3 * np.power(x, 2) - 9
+        result = self.optimizer.gradient_descent_RMSprop(df, np.array([2]), 0.01, 0.9, 200, 1e-8)
+        assert np.abs(result[-1] - np.sqrt(3)) < 1e-5
 
     def test_Adam(self):
-        df = lambda x: np.power(3 * x, 2) - 6 - 10
-        result = self.optimizer.gradient_descent_Adam(df, np.array([1.0, 2]), 0.01, 0.9, 0.99, 200, 1e-8)
+        df = lambda x: 3 * np.power(x, 2) - 9
+        result = self.optimizer.gradient_descent_Adam(df, np.array([2]), 0.01, 0.9, 0.99, 200, 1e-8)
 
-        print(np.max(result))
-        assert type(result) == list
+
+        assert np.abs(result[-1] - np.sqrt(3)) < 1e-5
