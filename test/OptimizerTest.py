0ce28a53d6db6a80904c62889836dbb0a0bcc77c
lovemefan <450489712@qq.com>
2021-04-05 15:33:53 +0800

    add Adam test

--- test/OptimizerTest.py
+++ test/OptimizerTest.py
@@ -32,3 +32,25 @@ class OptimizerTest(unittest.TestCase):
 
         result = self.optimizer.momentun(df, np.array([1.0, 2]), 0.01, 0.8, 20, 1e-8)
         assert type(result) == list
+
+    def test_adagrad(self):
+        df = lambda x: np.power(3 * x, 2) - 6 - 10
+        result = self.optimizer.gradient_descent_adagrad(df, np.array([1.0, 2]), 0.01, 200, 1e-8)
+        assert type(result) == list
+
+    def test_Adadelta(self):
+        df = lambda x: np.power(3 * x, 2) - 6 - 10
+        result = self.optimizer.gradient_descent_Adadelta(df, np.array([1.0, 2]), 0.01, 0.9, 200, 1e-8)
+        assert type(result) == list
+
+    def test_RMSprop(self):
+        df = lambda x: np.power(3 * x, 2) - 6 - 10
+        result = self.optimizer.gradient_descent_RMSprop(df, np.array([1.0, 2]), 0.01, 0.9, 200, 1e-8)
+        assert type(result) == list
+
+    def test_Adam(self):
+        df = lambda x: np.power(3 * x, 2) - 6 - 10
+        result = self.optimizer.gradient_descent_Adam(df, np.array([1.0, 2]), 0.01, 0.9, 0.99, 200, 1e-8)
+
+        print(np.max(result))
+        assert type(result) == list
